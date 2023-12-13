35e250f67699d021250093a975033f5d5f7d6455
yujhenchen <yujhen.chen93@gmail.com>
2023-12-13 09:39:52 +0800

    use em for padding

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 export default function Section({ children, customStyle = "" }) {
   return (
-    <section className={`w-full px-[46px] h-[320px] bg-white ${customStyle}`}>
+    <section className={`w-full px-[3em] h-[320px] bg-white ${customStyle}`}>
       {children}
     </section>
   );
